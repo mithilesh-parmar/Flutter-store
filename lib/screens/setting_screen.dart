6fcc449c7a44b4ca8335e58ec5fa6774f46fe4ac
Mithilesh Parmar <mithileshparmar1@gmail.com>
2019-08-26 11:14:44 +0530

    settings page updated

--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cool_store/states/app_state.dart';
+import 'package:cool_store/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -6,20 +7,42 @@ class SettingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final AppState appState = Provider.of<AppState>(context);
-    return Scaffold(
-      body: Container(
-        child: Center(
-          child: FlatButton(
-              onPressed: () {
-                appState.isDark
-                    ? appState.setLightTheme()
-                    : appState.setDarkTheme();
-              },
-              child: Text(
-                'Change Theme',
-              )),
+    return SafeArea(
+        child: CustomScrollView(
+      slivers: <Widget>[
+        SliverAppBar(
+          expandedHeight: Constants.screenAwareSize(80, context),
+          flexibleSpace: FlexibleSpaceBar(
+            background: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    'Setting',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Raleway',
+                        fontSize: 40),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 17, vertical: 8),
+                ),
+              ],
+            ),
+          ),
         ),
-      ),
-    );
+        SliverList(
+            delegate: SliverChildListDelegate([
+          ListTile(
+            onTap: () {
+              appState.isDark
+                  ? appState.setLightTheme()
+                  : appState.setDarkTheme();
+            },
+            leading: Icon(Icons.lightbulb_outline),
+            title: Text('Change Theme'),
+          )
+        ]))
+      ],
+    ));
   }
 }
